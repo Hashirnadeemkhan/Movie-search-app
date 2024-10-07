4b914c81ef9fc95693f405ebb49576ed38af8df5
Hashir Nadeem Khan <khashir657@gmail.com>
2024-10-07 11:59:38 +0500

    this is an movie search app where you can search any type of movie by give name of it

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['m.media-amazon.com'], // Add the domain here
+  },
+};
+
+export default nextConfig;
